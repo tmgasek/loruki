4204979e9199bc0d009c8b5ff1f54aedd58c1596
tmgasek <tomaszgasek28@gmail.com>
2021-11-27 13:33:41 +0000

    viewport

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,10 @@ export default function Navbar({ title }) {
     <div>
       <Head>
         <title>{title}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0"
+        ></meta>
       </Head>
 
       <div className="navbar">
